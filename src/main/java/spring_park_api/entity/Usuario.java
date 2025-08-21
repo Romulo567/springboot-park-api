fc4c9a55e68f3c76ca3b940fbdc1b52e738003c3
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-08-21 11:09:07 -0400

    atualizado a classe usuario

--- src/main/java/spring_park_api/entity/Usuario.java
+++ src/main/java/spring_park_api/entity/Usuario.java
@@ -12,12 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 @Entity
 @Table(name = "usuarios")
 public class Usuario implements Serializable{
@@ -33,7 +28,7 @@ public class Usuario implements Serializable{
 	@Column(name = "password", nullable = false, length = 200)
 	private String password;
 	@Enumerated(EnumType.STRING)
-	@Column(name = "role", nullable = false, length = 25)
+	@Column(name = "role", nullable = false, columnDefinition = "VARCHAR(20)")
 	private Role role;
 	
 	@Column(name = "data_criacao")
@@ -48,6 +43,85 @@ public class Usuario implements Serializable{
 	public enum Role{
 		ROLE_ADMIN,
 		ROLE_CLIENTE;
+	}
+
+	public Usuario() {
+	}
+	
+	public Usuario(Long id, String username, String password, Role role, LocalDateTime dataCriacao,
+			LocalDateTime dataModificacao, String criadoPor, String modificadoPor) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+		this.dataCriacao = dataCriacao;
+		this.dataModificacao = dataModificacao;
+		this.criadoPor = criadoPor;
+		this.modificadoPor = modificadoPor;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDateTime dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public LocalDateTime getDataModificacao() {
+		return dataModificacao;
+	}
+
+	public void setDataModificacao(LocalDateTime dataModificacao) {
+		this.dataModificacao = dataModificacao;
+	}
+
+	public String getCriadoPor() {
+		return criadoPor;
+	}
+
+	public void setCriadoPor(String criadoPor) {
+		this.criadoPor = criadoPor;
+	}
+
+	public String getModificadoPor() {
+		return modificadoPor;
+	}
+
+	public void setModificadoPor(String modificadoPor) {
+		this.modificadoPor = modificadoPor;
 	}
 
 	@Override
@@ -71,5 +145,4 @@ public class Usuario implements Serializable{
 	public String toString() {
 		return "Usuario [id=" + id + "]";
 	}
-
 }
