3c062a855acb300590440ac383cabdbb8686b4ba
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-08-21 11:10:34 -0400

    criacao de usuario

--- src/main/java/spring_park_api/service/UsuarioService.java
+++ src/main/java/spring_park_api/service/UsuarioService.java
@@ -3,11 +3,18 @@ package spring_park_api.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+import spring_park_api.entity.Usuario;
 import spring_park_api.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
 
 	@Autowired
-	private UsuarioRepository usarioRepository;
+	private UsuarioRepository usuarioRepository;
+
+	@Transactional
+	public Usuario create(Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
 }
--- src/main/java/spring_park_api/web/controller/UsuarioController.java
+++ src/main/java/spring_park_api/web/controller/UsuarioController.java
@@ -1,9 +1,14 @@
 package spring_park_api.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import spring_park_api.entity.Usuario;
 import spring_park_api.service.UsuarioService;
 
 @RestController
@@ -12,4 +17,10 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@PostMapping
+	public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+		usuario = usuarioService.create(usuario);
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+	}
 }
