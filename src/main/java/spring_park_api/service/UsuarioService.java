5aaa5b4ad8d6cae25ed9af2979188baa1ec4279f
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-08-21 13:47:24 -0400

    Buscando usuario por id

--- src/main/java/spring_park_api/service/UsuarioService.java
+++ src/main/java/spring_park_api/service/UsuarioService.java
@@ -2,8 +2,8 @@ package spring_park_api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.transaction.Transactional;
 import spring_park_api.entity.Usuario;
 import spring_park_api.repository.UsuarioRepository;
 
@@ -14,7 +14,12 @@ public class UsuarioService {
 	private UsuarioRepository usuarioRepository;
 
 	@Transactional
-	public Usuario create(Usuario usuario) {
+	public Usuario criar(Usuario usuario) {
 		return usuarioRepository.save(usuario);
+	}
+	
+	@Transactional(readOnly = true)
+	public Usuario buscarPorId(Long id) {
+		return usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
 	}
 }
--- src/main/java/spring_park_api/web/controller/UsuarioController.java
+++ src/main/java/spring_park_api/web/controller/UsuarioController.java
@@ -3,6 +3,8 @@ package spring_park_api.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +22,13 @@ public class UsuarioController {
 	
 	@PostMapping
 	public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
-		usuario = usuarioService.create(usuario);
+		usuario = usuarioService.criar(usuario);
 		return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> readById(@PathVariable Long id){
+		Usuario user = usuarioService.buscarPorId(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
