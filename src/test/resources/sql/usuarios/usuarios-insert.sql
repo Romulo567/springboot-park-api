47eaa374c4f9bf4feafae007220ddc1bd63133a0
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-08-28 10:00:53 -0400

    Classe de config SpringSecurity

--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,3 +1,3 @@
-insert into USUARIOs (id, username, password, role) values (100, 'ana@gmail.com', '123456', 'ROLE_ADMIN');
-insert into USUARIOs (id, username, password, role) values (101, 'kelvin@gmail.com', '123456', 'ROLE_CLIENTE');
-insert into USUARIOs (id, username, password, role) values (102, 'vitor@gmail.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (100, 'ana@gmail.com', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (101, 'kelvin@gmail.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (102, 'vitor@gmail.com', '123456', 'ROLE_CLIENTE');
