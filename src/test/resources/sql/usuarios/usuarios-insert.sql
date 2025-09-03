3e6c756a4ad9802ac9c8b4fbba4a3f1fcd5ca227
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-09-03 09:49:21 -0400

    Teste com autenticaçao no buscar usuario por id com status 200

--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,3 +1,3 @@
-insert into USUARIOS (id, username, password, role) values (100, 'ana@gmail.com', '123456', 'ROLE_ADMIN');
-insert into USUARIOS (id, username, password, role) values (101, 'kelvin@gmail.com', '123456', 'ROLE_CLIENTE');
-insert into USUARIOS (id, username, password, role) values (102, 'vitor@gmail.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (100, 'ana@gmail.com', '$2a$10$jTA9/NKrBRQJ9B7FdfJzzeymVBZ.bBhW7igl4gvBaLd3q3BfiP.O2', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (101, 'kelvin@gmail.com', '$2a$10$jTA9/NKrBRQJ9B7FdfJzzeymVBZ.bBhW7igl4gvBaLd3q3BfiP.O2', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (102, 'vitor@gmail.com', '$2a$10$jTA9/NKrBRQJ9B7FdfJzzeymVBZ.bBhW7igl4gvBaLd3q3BfiP.O2', 'ROLE_CLIENTE');
