44307bcee83e6680624b597563e02aa9cfad6231
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-09-03 10:29:26 -0400

    Teste com autenticaçao para editar senha com status 204 e 403

--- src/main/java/spring_park_api/web/controller/UsuarioController.java
+++ src/main/java/spring_park_api/web/controller/UsuarioController.java
@@ -73,10 +73,7 @@ public class UsuarioController {
 	@Operation(summary = "Atualizar senha", description = "Requisição exige um Bearer token. Acesso restrito a ADMIN|CLIENTE",
 			security = @SecurityRequirement(name = "security"),
 			responses = {
-				@ApiResponse(responseCode = "204", description = "Senha atualizada com sucesso", 
-						content = @Content(mediaType = "application/json", schema = @Schema(implementation = void.class))),
-				@ApiResponse(responseCode = "404", description = "Recurso não encontrado", 
-						content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+				@ApiResponse(responseCode = "204", description = "Senha atualizada com sucesso" ),
 				@ApiResponse(responseCode = "400", description = "Senha não confere", 
 						content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
 				@ApiResponse(responseCode = "422", description = "Campos invalidos ou mal formatados", 
--- src/test/java/spring_park_api/UsuarioIT.java
+++ src/test/java/spring_park_api/UsuarioIT.java
@@ -218,31 +218,57 @@ public class UsuarioIT {
 	}
 	
 	@Test
-	public void editarSenha_ComDadosdValidos_RetornarStatus404() {
+	public void editarSenha_ComDadosdValidos_RetornarStatus204() {
 		 testClient
-			.patch()
-			.uri("/api/v1/usuarios/100")
-			.contentType(MediaType.APPLICATION_JSON)
-			.bodyValue(new UsuarioSenhaDto("123456", "123456", "123456"))
-			.exchange()
-			.expectStatus()
-			.isNoContent();
+				.patch()
+				.uri("/api/v1/usuarios/100")
+				.headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@gmail.com", "123456"))
+				.contentType(MediaType.APPLICATION_JSON)
+				.bodyValue(new UsuarioSenhaDto("123456", "123456", "123456"))
+				.exchange()
+				.expectStatus()
+				.isNoContent();
+		 
+		 testClient
+				.patch()
+				.uri("/api/v1/usuarios/101")
+				.headers(JwtAuthentication.getHeaderAuthorization(testClient, "kelvin@gmail.com", "123456"))
+				.contentType(MediaType.APPLICATION_JSON)
+				.bodyValue(new UsuarioSenhaDto("123456", "123456", "123456"))
+				.exchange()
+				.expectStatus()
+				.isNoContent();
 	}
 	
 	@Test
-	public void editarSenha_ComIdInexistente_RetornarErrorMessageComStatus404() {
+	public void editarSenha_ComUsuariosDiferentes_RetornarErrorMessageComStatus403() {
 		ErrorMessage responseBody = testClient
-			.patch()
-			.uri("/api/v1/usuarios/0")
-			.contentType(MediaType.APPLICATION_JSON)
-			.bodyValue(new UsuarioSenhaDto("123456", "123456", "123456"))
-			.exchange()
-			.expectStatus().isNotFound()
-			.expectBody(ErrorMessage.class)
-			.returnResult().getResponseBody();
+				.patch()
+				.uri("/api/v1/usuarios/0")
+				.headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@gmail.com", "123456"))
+				.contentType(MediaType.APPLICATION_JSON)
+				.bodyValue(new UsuarioSenhaDto("123456", "123456", "123456"))
+				.exchange()
+				.expectStatus().isForbidden()
+				.expectBody(ErrorMessage.class)
+				.returnResult().getResponseBody();
 		
 		org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
-		org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(404);
+		org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
+		
+		responseBody = testClient
+					.patch()
+					.uri("/api/v1/usuarios/0")
+					.headers(JwtAuthentication.getHeaderAuthorization(testClient, "kelvin@gmail.com", "123456"))
+					.contentType(MediaType.APPLICATION_JSON)
+					.bodyValue(new UsuarioSenhaDto("123456", "123456", "123456"))
+					.exchange()
+					.expectStatus().isForbidden()
+					.expectBody(ErrorMessage.class)
+					.returnResult().getResponseBody();
+			
+			org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+			org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
 	}
 	
 	@Test
