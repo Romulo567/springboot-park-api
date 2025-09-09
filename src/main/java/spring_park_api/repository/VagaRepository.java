6e1b7f33fbb215f8a3a8409509c84a42a048cb97
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-09-09 11:05:33 -0400

    Criando e mapeando entidade Vaga e criando repositorio

---
+++ src/main/java/spring_park_api/repository/VagaRepository.java
@@ -0,0 +1,9 @@
+package spring_park_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import spring_park_api.entity.Vaga;
+
+public interface VagaRepository extends JpaRepository<Vaga, Long>{
+
+}
