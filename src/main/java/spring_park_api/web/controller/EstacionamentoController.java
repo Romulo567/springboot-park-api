43f14b1ed6dc9a34711e9c8bbcce4405d0227ee6
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-09-17 09:37:34 -0400

    Documentando recurso de localizar estacionamentos por cpf

--- src/main/java/spring_park_api/web/controller/EstacionamentoController.java
+++ src/main/java/spring_park_api/web/controller/EstacionamentoController.java
@@ -23,6 +23,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -133,6 +134,31 @@ public class EstacionamentoController {
 		return ResponseEntity.ok(dto);
 	}
 	
+	@Operation(summary = "Localizar os registros de estacionamentos do cliente por cpf", description = "Localizar os  " +
+			"registros de estacionamentos do cliente por CPF. Requisição exige uso de um bearer token.''",
+			security = @SecurityRequirement(name = "security"),
+			parameters = {
+					@Parameter(in = ParameterIn.PATH, name = "cpf", description = "Número do cpf referente ao cliente a ser consultado", required = true
+							),
+					@Parameter(in = ParameterIn.QUERY, name = "page", description = "Representa a pagina retornada",
+									content = @Content(schema = @Schema(type = "integer", defaultValue = "0"))
+							),
+					@Parameter(in = ParameterIn.QUERY, name = "size", description = "Representa o total de elementos por pagina",
+									content = @Content(schema = @Schema(type = "integer", defaultValue = "5"))
+							),
+					@Parameter(in = ParameterIn.QUERY, name = "sort", description = "Campo padrão de ordenação 'dataEntrada,asc'. ",
+									array = @ArraySchema(schema = @Schema(type = "string", defaultValue = "dataEntrada,asc")),
+									hidden = true
+							)
+			},
+			responses = {
+					@ApiResponse(responseCode = "200", description = "Recurso localizado com sucesso",
+							content = @Content(mediaType = "application/json;charset=UTF-8",
+									schema = @Schema(implementation = PageableDto.class))),
+					@ApiResponse(responseCode = "403", description = "Rescurso não permitido ao perfil de CLIENTE", 
+							content = @Content(mediaType = "application/json;charset=UTF-8",
+									schema = @Schema(implementation = ErrorMessage.class))),
+			})
 	@GetMapping("/cpf/{cpf}")
 	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<PageableDto> getAllEstacionamentosPorCpf(@PathVariable String cpf, 
